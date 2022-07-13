a69fc6f0d42488f86d4b89bc6c530c0a1fd193e3
zyeongPark <piiy10141997@gmail.com>
2022-07-13 21:45:51 +0900

    Initial Commit

---
+++ Algorithm/BOJ/step_1/18108.swift
@@ -0,0 +1,11 @@
+//
+//  18108.swift
+//  Algorithm
+//
+//  Created by 박지영 on 2022/07/13.
+//
+
+import Foundation
+
+var year = readLine()!
+print(Int(year)! - 543)
---
+++ Algorithm/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 박지영 on 2022/07/13.
+//
+
+import Foundation
+
+
+var greeting = "hello "
