cf93d0fc341d3804851db7154d2a6ea664659fdc
zyeongPark <piiy10141997@gmail.com>
2022-08-10 10:52:42 +0900

    0810

--- Algorithm/LeetCode/day13/1.swift
+++ Algorithm/LeetCode/day13/1.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+//생각나는 대로 짠 코드: 2중 for문 -> 시간복잡도: o(n2)
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        for i in 0..<nums.count {
+            for j in i+1..<nums.count {
+                if nums[i] + nums[j] == target {
+                    return [i,j]
+                }
+            }
+        }
+        return []
+    }
+}
+
+//딕셔너리 사용 코드 (참고 링크 확인) -> 시간복잡도: o(n)
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+
+    var dics = [Int:Int]()
+
+    for i in 0..<nums.count {
+        dics[nums[i]] = i
+    }
+
+    for j in 0..<nums.count {
+        let complement = target - nums[j] // 보수 구하기
+                //보수는 보충해주는 수 (예를 들어 10을 만든다고 할 때 1의 보수는 9)
+        let cIdx = dics[complement] //보수에 해당하는 nums의 인덱스값 가져오기
+        if cIdx != nil && cIdx != j { return [cIdx!, j]}
+    }
+    return []
+}
--- Algorithm/LeetCode/day13/299.swift
+++ Algorithm/LeetCode/day13/299.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+//testcase 중에 "1123" "0111" 만족시키지 못했음(다시 풀어보기) 
+class Solution {
+    func getHint(_ secret: String, _ guess: String) -> String {
+        var bulls = 0
+        var cows = 0
+        
+        for i in 0..<secret.count{
+            var x = secret.prefix(i)
+            for j in 0..<guess.count{
+                var y = guess.prefix(i)
+                if x == y{
+                    if i == j{
+                        bulls += 1
+                    }else{
+                        cows += 1
+                    }
+                }else{
+                    continue
+                }
+            }
+        }
+        return String(bulls) + "A" + String(cows) + "B"
+    }
+}
