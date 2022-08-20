3c8f6ed333d723e5d4ce685dbb247793a0250055
zyeongPark <piiy10141997@gmail.com>
2022-08-20 20:19:53 +0900

    0820

--- Algorithm/BOJ/step_5/4673.swift
+++ Algorithm/BOJ/step_5/4673.swift
@@ -8,4 +8,19 @@
 import Foundation
 //도저히 모르겠어서 구글링함.
 
+var s: Set<Int> = []
+for i in 1...10000 {
+    s.insert(d(i))
+}
+for j in 1...10000 {
+    if !s.contains(j) { print(j) }
+}
+func d(_ n:Int) -> Int {
+    var sum = n, now = n
+    while now != 0 {
+        sum += now%10
+        now /= 10
+    }
+    return sum
+}
 
