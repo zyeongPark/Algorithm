b74baebd7fedf8b92a193581a246e8728c184b33
zyeongPark <piiy10141997@gmail.com>
2022-08-22 15:28:36 +0900

    0822

--- Algorithm/programmers/level2/featureDevelop.swift
+++ Algorithm/programmers/level2/featureDevelop.swift
@@ -4,9 +4,9 @@
 //
 //  Created by 박지영 on 2022/08/21.
 //
-//5:40 ~ 6:15 (25분 걸림)
-import Foundation
 
+import Foundation
+//5:40 ~ 6:15 (25분 걸림)
 func solution(_ progress:[Int], _ speeds:[Int]) -> [Int] {
     var left_progress: [Int] = []
     var day:[Int] = []
@@ -44,42 +44,3 @@ func solution(_ progress:[Int], _ speeds:[Int]) -> [Int] {
     }
     return result
 }
-
-//구글링한 방법 중 제일 와닿았던 방법
-func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
-  var feature = progresses
-  var speed = speeds
-  var answer: [Int] = []
-  
-  repeat {
-    var count = 0
-    
-    //feature(progresses)를 speeds만큼 더한다
-    for idx in 0..<feature.count {
-      feature[idx] += speed[idx]
-    }
-
-    repeat {
-      // feature의 첫번째 값이 100이상인지 체크하고,
-      if feature[0] >= 100 {
-        // 100이상이 경우, feature의 값과 speed의 값 삭제 후 count 세기
-        feature.removeFirst()
-        speed.removeFirst()
-        count += 1
-      } else {
-        break
-      }
-      // feature의 첫번째 값이 100이상이 아닐 때까지 반복해준다
-      // 100이상이 아닐 경우, break로 repeat-while문을 탈출
-    } while !feature.isEmpty
-    
-    //count가 0이상인 경우(배포한 경우), answer에 해당 값을 담는다
-    if count > 0 {
-      answer.append(count)
-    }
-    //feature에 값이 없을 때 까지 위 모든 과정을 반복
-  } while !feature.isEmpty
-    
-  return answer
-}
-
