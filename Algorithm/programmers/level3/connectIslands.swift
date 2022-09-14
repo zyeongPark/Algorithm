aa20d6f88c1d445a3a401794715bd16d6cea59eb
zyeongPark <piiy10141997@gmail.com>
2022-09-14 11:43:12 +0900

    0914

--- Algorithm/programmers/level3/connectIslands.swift
+++ Algorithm/programmers/level3/connectIslands.swift
@@ -4,5 +4,47 @@
 //
 //  Created by 박지영 on 2022/09/08.
 //
+
 import Foundation
 
+func solution(_ n:Int, _ costs:[[Int]]) -> Int {
+    var result = 0
+    //부모 n+1 개수만큼 만들어놓고
+    var parent:[Int] = [Int](repeating:0, count: n+1)
+    //cost오름차순으로 정렬함
+    let sortedCost = costs.sorted { $0[2] < $1[2] }
+    
+    //일단 부모노드를 자기 자신으로 만들어둠
+    for i in 1..<n+1{
+        parent[i] = i
+    }
+    
+    for cost in sortedCost{
+        //부모노드의 사이클 없을 때만! union해주기
+        if findParent(&parent, cost[0]) != findParent(&parent, cost[1]){
+            unionParent(&parent, cost[0], cost[1])
+            result += cost[2]
+        }
+    }
+    return result
+}
+//부모노드 합치기
+func unionParent(_ parent: inout [Int], _ x: Int, _ y:Int){
+    //1. 각각의 부모노드 찾은다음에
+    let x = findParent(&parent, x)
+    let y = findParent(&parent, y)
+    //2. 크기 비교해서 부모노드 바꿔주기
+    if x < y{
+        parent[y] = x
+    } else{
+        parent[x] = y
+    }
+}
+
+//부모노드 찾기
+func findParent(_ parent: inout [Int], _ p: Int) -> Int {
+    if parent[p] != p {
+        parent[p] = findParent(&parent, parent[p])
+    }
+    return parent[p]
+}
--- Algorithm/programmers/level3/expressByN.swift
+++ Algorithm/programmers/level3/expressByN.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+func solution(_ N:Int, _ number:Int) -> Int {
+    if N == number {
+        return 1
+    }
+    //count가 8보다 크면 바로 -1 리턴하기용
+    var answer: Int = -1
+        
+    func dfs(_ count: Int, _ now: Int) {
+        if count > 8 { return }
+        if now == number {
+            //현재 숫자인 now와 number가 같다면 종료, 여기서 answer는 count의 최소값으로 업데이트
+            if count < answer || answer == -1 {
+                answer = count
+            }
+            return
+        }
+        
+        var n = 0
+        for i in 0..<8 {
+            if (answer != -1) && (answer < count + 1 + i) {
+                break
+            }
+            //555 -> 5 인 경우 n이 555 -> 55 -> 5 가 될 수 있도록 n*10 +n 진행
+            n = (n * 10) + N
+            dfs(count + 1 + i, now + n)
+            dfs(count + 1 + i, now - n)
+            dfs(count + 1 + i, now * n)
+            dfs(count + 1 + i, now / n)
+        }
+    }
+    
+    dfs(0, 0)
+    
+    return answer
+}
