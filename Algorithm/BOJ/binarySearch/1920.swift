c2d8d08c618b6350fd3d72a3fa49ae9fa7e4a067
zyeongPark <piiy10141997@gmail.com>
2022-09-28 11:03:54 +0900

    0928

--- Algorithm/BOJ/binarySearch/1920.swift
+++ Algorithm/BOJ/binarySearch/1920.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+//입력받기
+let n = Int(readLine()!)!
+var firstArr = readLine()!.split(separator: " ").map{Int($0)!}
+let m = Int(readLine()!)!
+var secondArr = readLine()!.split(separator: " ").map{Int($0)!}
+//정렬하기
+firstArr.sort()
+//메인로직
+func binarySearch(_ arr: [Int], _ target: Int) -> Int{
+    var lo = 0
+    var hi = arr.count - 1
+
+    while (lo <= hi) {
+        let mid = (lo + hi) / 2
+            if firstArr[mid] == target {
+                return 1
+            }else if firstArr[mid] > target {
+                hi = mid - 1
+            }else if firstArr[mid] < target {
+                lo = mid + 1
+            }
+        }
+        return 0
+}
+
+for i in 0..<m {
+    print(binarySearch(firstArr, secondArr[i]))
+}
