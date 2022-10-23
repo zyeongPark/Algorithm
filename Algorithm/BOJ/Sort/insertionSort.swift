81a6f66f61686006526583b5d86f0bc726bdf02d
zyeongPark <piiy10141997@gmail.com>
2022-10-24 08:50:52 +0900

    1024

--- Algorithm/BOJ/Sort/insertionSort.swift
+++ Algorithm/BOJ/Sort/insertionSort.swift
@@ -7,3 +7,26 @@
 
 import Foundation
 //삽입 정렬 정리하기
+input = [8,6,5,2,4]
+func insertionSort<T: Comparable>(_ array: [T]) -> [T] {
+    var sort = input
+    for i in 1..<input.count {
+        var j = i
+        let temp = input[j]
+        while (j > 0 && temp < input[j - 1]) {
+            input[j] = input[j - 1]
+          j -= 1
+        }
+        
+        input[j] = temp
+    }
+    return sort
+}
+print(insertionSort((input)))
+
+단순 but 비효율
+시간복잡도 : best(이동 없이 1번의 비교만) - o(n) & worst (각 반복마다 i번 비교) - O(n^2)
+공간복잡도 : 주어진 배열 안에서만 교환 - o(n)
+inplace : true (추가적인 메모리 공간 거의 필요 x)
+stable
+
