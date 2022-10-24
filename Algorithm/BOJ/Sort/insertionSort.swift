9d2c71eb6a736129f67128090d7eae7c0d11d974
zyeongPark <piiy10141997@gmail.com>
2022-10-24 09:05:17 +0900

    1024

--- Algorithm/BOJ/Sort/insertionSort.swift
+++ Algorithm/BOJ/Sort/insertionSort.swift
@@ -7,26 +7,28 @@
 
 import Foundation
 //삽입 정렬 정리하기
-input = [8,6,5,2,4]
-func insertionSort<T: Comparable>(_ array: [T]) -> [T] {
-    var sort = input
+var input = [8,6,5,2,4]
+func insertionSort<T: Comparable>(_ input: [T]) -> [T] {
+    var sortedArray = input
     for i in 1..<input.count {
         var j = i
-        let temp = input[j]
-        while (j > 0 && temp < input[j - 1]) {
-            input[j] = input[j - 1]
+        let temp = sortedArray[j]
+        while (j > 0 && temp < sortedArray[j - 1]) {
+            sortedArray[j] = sortedArray[j - 1]
           j -= 1
         }
-        
-        input[j] = temp
+
+        sortedArray[j] = temp
     }
-    return sort
+    return sortedArray
 }
+
 print(insertionSort((input)))
 
-단순 but 비효율
-시간복잡도 : best(이동 없이 1번의 비교만) - o(n) & worst (각 반복마다 i번 비교) - O(n^2)
-공간복잡도 : 주어진 배열 안에서만 교환 - o(n)
-inplace : true (추가적인 메모리 공간 거의 필요 x)
-stable
+//선택정렬 특징
+//단순 but 비효율
+//시간복잡도 : best(이동 없이 1번의 비교만) - o(n) & worst (각 반복마다 i번 비교) - O(n^2)
+//공간복잡도 : 주어진 배열 안에서만 교환 - o(n)
+//inplace : true (추가적인 메모리 공간 거의 필요 x)
+//stable
 
--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -8,133 +8,134 @@
 
 import Foundation
 //열목이님 코드 다시보기
-struct Node {
-    let number: Int
-    let weight: Int
-}
-struct MinHeap {
-    var heap = [Node(number: 0, weight: 0)]
+//struct Node {
+//    let number: Int
+//    let weight: Int
+//}
+//struct MinHeap {
+//    var heap = [Node(number: 0, weight: 0)]
+//
+//    var isEmpty: Bool { return heap.count == 1 }
+//
+//    func parentIndex(_ index: Int) -> Int {
+//        index / 2
+//    }
+//
+//    private func leftChildIndex(of index: Int) -> Int {
+//        index * 2
+//    }
+//
+//    private func rightChildIndex(of index: Int) -> Int {
+//        index * 2 + 1
+//    }
+//
+//    mutating func enqueue(_ element: Node) {
+//        if heap.count == 1 {
+//            heap.append(element)
+//            return
+//        }
+//        heap.append(element)
+//
+//        restructureForPush()
+//    }
+//    mutating func restructureForPush() {
+//        var index = heap.count - 1
+//
+//        while index != 1 && heap[index].weight < heap[parentIndex(index)].weight {
+//            heap.swapAt(index, parentIndex(index))
+//            index /= 2
+//        }
+//    }
+//
+//    mutating func dequeue() -> Node? {
+//        guard heap.count > 1 else { return nil}
+//
+//        if heap.count == 2 {
+//            return heap.removeLast()
+//        }
+//
+//        let minValueNode = heap[1]
+//        heap[1] = heap.removeLast()
+//
+//        restructureForPop()
+//
+//        return minValueNode
+//    }
+//
+//    mutating func restructureForPop() {
+//        var index = 1
+//        while rightChildIndex(of: index) < heap.count {
+//            if heap[index].weight <= min(heap[leftChildIndex(of: index)].weight, heap[rightChildIndex(of: index)].weight) {
+//                break
+//            } else if heap[leftChildIndex(of: index)].weight < heap[rightChildIndex(of: index)].weight {
+//                heap.swapAt(index, leftChildIndex(of: index))
+//                index = leftChildIndex(of: index)
+//            } else {
+//                heap.swapAt(index, rightChildIndex(of: index))
+//                index = rightChildIndex(of: index)
+//            }
+//        }
+//
+//        if leftChildIndex(of: index) < heap.count && heap[index].weight > heap[leftChildIndex(of: index)].weight {
+//            heap.swapAt(index, leftChildIndex(of: index))
+//        }
+//    }
+//}
+//
+//let infinity = Int(1e9)
+//let inputData = readLine()!.components(separatedBy: " ").map { Int($0)! }
+//let (numberOfVertices, numberOfEdges) = (inputData[0], inputData[1])
+//
+//let startVertex = Int(readLine()!)!
+//
+//var graph = Array(repeating: Array(repeating: infinity, count: numberOfVertices + 1), count: numberOfVertices + 1)
+//var visited = Array(repeating: false, count: numberOfVertices + 1)
+//
+//var minWeight = Array(repeating: infinity, count: numberOfVertices + 1)
+//var priorityQueue = MinHeap()
+//
+//for index in 1...numberOfVertices {
+//    graph[index][index] = 0
+//}
+//
+//for _ in 0..<numberOfEdges {
+//    let inputData = readLine()!.components(separatedBy: " ").map { Int($0)! }
+//    let (departure, destination, weight) = (inputData[0], inputData[1], inputData[2])
+//
+//    graph[departure][destination] = weight
+//}
+//
+//
+//func solution(_ start: Int) {
+//    priorityQueue.enqueue(Node(number: start, weight: graph[start][start]))
+//
+//    minWeight[start] = 0
+//
+//    while !priorityQueue.isEmpty {
+//        guard let now = priorityQueue.dequeue() else { return }
+//
+//        if minWeight[now.number] < now.weight {
+//            continue
+//        }
+//        // now를 경유지로 해서, 처음~마지막 노드를 목적지로 삼아서 기존의 비용보다 적게 든다면 갱신, 우선순위 큐에 삽입
+//        for destination in 1...numberOfVertices {
+//            let viaWeight = minWeight[now.number] + graph[now.number][destination]
+//            if viaWeight < minWeight[destination] {
+//                minWeight[destination] = viaWeight
+//                priorityQueue.enqueue(Node(number: destination, weight: viaWeight))
+//            }
+//        }
+//    }
+//}
+//
+//solution(startVertex)
+//
+//for index in 1...numberOfVertices {
+//    if minWeight[index] == infinity {
+//        print("INF")
+//    } else {
+//        print(minWeight[index])
+//    }
+//}
 
-    var isEmpty: Bool { return heap.count == 1 }
-
-    func parentIndex(_ index: Int) -> Int {
-        index / 2
-    }
-
-    private func leftChildIndex(of index: Int) -> Int {
-        index * 2
-    }
-
-    private func rightChildIndex(of index: Int) -> Int {
-        index * 2 + 1
-    }
-
-    mutating func enqueue(_ element: Node) {
-        if heap.count == 1 {
-            heap.append(element)
-            return
-        }
-        heap.append(element)
-
-        restructureForPush()
-    }
-    mutating func restructureForPush() {
-        var index = heap.count - 1
-
-        while index != 1 && heap[index].weight < heap[parentIndex(index)].weight {
-            heap.swapAt(index, parentIndex(index))
-            index /= 2
-        }
-    }
-
-    mutating func dequeue() -> Node? {
-        guard heap.count > 1 else { return nil}
-
-        if heap.count == 2 {
-            return heap.removeLast()
-        }
-
-        let minValueNode = heap[1]
-        heap[1] = heap.removeLast()
-
-        restructureForPop()
-
-        return minValueNode
-    }
-
-    mutating func restructureForPop() {
-        var index = 1
-        while rightChildIndex(of: index) < heap.count {
-            if heap[index].weight <= min(heap[leftChildIndex(of: index)].weight, heap[rightChildIndex(of: index)].weight) {
-                break
-            } else if heap[leftChildIndex(of: index)].weight < heap[rightChildIndex(of: index)].weight {
-                heap.swapAt(index, leftChildIndex(of: index))
-                index = leftChildIndex(of: index)
-            } else {
-                heap.swapAt(index, rightChildIndex(of: index))
-                index = rightChildIndex(of: index)
-            }
-        }
-
-        if leftChildIndex(of: index) < heap.count && heap[index].weight > heap[leftChildIndex(of: index)].weight {
-            heap.swapAt(index, leftChildIndex(of: index))
-        }
-    }
-}
-
-let infinity = Int(1e9)
-let inputData = readLine()!.components(separatedBy: " ").map { Int($0)! }
-let (numberOfVertices, numberOfEdges) = (inputData[0], inputData[1])
-
-let startVertex = Int(readLine()!)!
-
-var graph = Array(repeating: Array(repeating: infinity, count: numberOfVertices + 1), count: numberOfVertices + 1)
-var visited = Array(repeating: false, count: numberOfVertices + 1)
-
-var minWeight = Array(repeating: infinity, count: numberOfVertices + 1)
-var priorityQueue = MinHeap()
-
-for index in 1...numberOfVertices {
-    graph[index][index] = 0
-}
-
-for _ in 0..<numberOfEdges {
-    let inputData = readLine()!.components(separatedBy: " ").map { Int($0)! }
-    let (departure, destination, weight) = (inputData[0], inputData[1], inputData[2])
-
-    graph[departure][destination] = weight
-}
-
-
-func solution(_ start: Int) {
-    priorityQueue.enqueue(Node(number: start, weight: graph[start][start]))
-
-    minWeight[start] = 0
-
-    while !priorityQueue.isEmpty {
-        guard let now = priorityQueue.dequeue() else { return }
-
-        if minWeight[now.number] < now.weight {
-            continue
-        }
-        // now를 경유지로 해서, 처음~마지막 노드를 목적지로 삼아서 기존의 비용보다 적게 든다면 갱신, 우선순위 큐에 삽입
-        for destination in 1...numberOfVertices {
-            let viaWeight = minWeight[now.number] + graph[now.number][destination]
-            if viaWeight < minWeight[destination] {
-                minWeight[destination] = viaWeight
-                priorityQueue.enqueue(Node(number: destination, weight: viaWeight))
-            }
-        }
-    }
-}
-
-solution(startVertex)
-
-for index in 1...numberOfVertices {
-    if minWeight[index] == infinity {
-        print("INF")
-    } else {
-        print(minWeight[index])
-    }
-}
 
