7e24877bea316c7be507f2ae8d9b16084042d41f
zyeongPark <piiy10141997@gmail.com>
2022-11-15 09:35:52 +0900

    1115_캐시문제

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -7,7 +7,35 @@
 // 4673, 10951, 2439번
 
 import Foundation
-//백준 - 문제풀이
+//카카오- 2018(1차 캐시)
+
+import Foundation
+
+func solution(_ cacheSize:Int, _ cities:[String]) -> Int {
+    
+    let newCitiies = cities.map {
+        return $0.lowercased()
+    }
+
+    var cacheArray = [String]()
+    var totalTime = 0
+
+    for city in newCitiies {
+        // cache hit
+        if cacheArray.contains(city) {
+            totalTime += 1
+            cacheArray.remove(at: cacheArray.firstIndex(of: city)!)
+            cacheArray.append(city)
+        } else { // cache miss
+            totalTime += 5
+            cacheArray.append(city)
+            if cacheArray.count > cacheSize {
+                cacheArray.removeFirst()
+            }
+        }
+    }
+    return totalTime
+}
 
 
 
