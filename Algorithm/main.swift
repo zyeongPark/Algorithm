8335081d72c2f10d0d833abfc6fec987f6541ce8
zyeongPark <piiy10141997@gmail.com>
2022-11-16 22:20:29 +0900

    1116

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -7,35 +7,52 @@
 // 4673, 10951, 2439번
 
 import Foundation
-//카카오- 2018(1차 캐시)
 
-import Foundation
-
-func solution(_ cacheSize:Int, _ cities:[String]) -> Int {
+func solution(_ s:String) -> Int {
+    let str = s.map { String($0) }
     
-    let newCitiies = cities.map {
-        return $0.lowercased()
-    }
-
-    var cacheArray = [String]()
-    var totalTime = 0
-
-    for city in newCitiies {
-        // cache hit
-        if cacheArray.contains(city) {
-            totalTime += 1
-            cacheArray.remove(at: cacheArray.firstIndex(of: city)!)
-            cacheArray.append(city)
-        } else { // cache miss
-            totalTime += 5
-            cacheArray.append(city)
-            if cacheArray.count > cacheSize {
-                cacheArray.removeFirst()
+    var minLength = str.count
+    for size in (0..<str.count / 2).reversed() {
+        let cutSize = size + 1
+        
+        var temp = [String](str[0..<cutSize])
+        var count = -1
+        var result = [String]()
+        
+        for index in (0..<str.count / cutSize) {
+            let startIndex = index * cutSize
+            let current = [String](str[startIndex..<startIndex + cutSize])
+            if current == temp {
+                count += 1
+                temp = current
+            } else {
+                if count == 0 {
+                    result += temp
+                    temp = current
+                } else {
+                    result += String(count + 1).map { String($0) }
+                    result += temp
+                    count = 0
+                    temp = current
+                }
             }
         }
+        if count != 0 {
+            result += String(count + 1).map { String($0) }
+        }
+        result += temp
+        
+        // 남은 나머지들은 이어서 붙이기
+        let remains = str[(str.count / cutSize * cutSize)...]
+        result += remains
+        if result.count < minLength {
+            minLength = result.count
+        }
     }
-    return totalTime
+    
+    return minLength
 }
+
 
 
 
